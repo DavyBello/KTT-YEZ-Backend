5f2c10ad1693b1a348e525a4a605abb23368f619
Bello Oladipupo <bellooladipupo41@gmail.com>
2018-03-24 18:23:00 +0100

    created signUp resolver

--- graphql/resolvers/candidate.js
+++ graphql/resolvers/candidate.js
@@ -40,4 +40,68 @@ module.exports = () => {
     },
   })
 
+  CandidateTC.addResolver({
+    kind: 'mutation',
+    name: 'signUp',
+    description: 'signUp a candidate',
+    args: {firstName: 'String!', lastName: 'String!', phone: 'String!', password: 'String!'},
+    type: CandidateTC,
+    resolve: async ({ args, context }) => {
+      // console.log('candidate signUp this ----');
+      const { firstName, lastName, phone, password } = args;
+
+      return Candidate.findOne({phone}).then((existing) => {
+        if (!existing) {
+          // hash password and create user
+          const newCandidate = new Candidate({
+            phone,
+            password: password,
+            name: {
+              first: firstName,
+              last: lastName
+            }
+          })
+          return newCandidate.save().then((candidate)=>{
+            const { id, phone } = candidate;
+            //console.log('---' + hash);
+            const token = jwt.sign({
+              id: candidate.id,
+              //email: candidate.email,
+              phone: candidate.phone,
+              type: 'Candidate',
+              //passwordVersion: candidate.passwordVersion,
+            }, process.env.JWT_SECRET);
+            console.log('-----' + candidate.password);
+            candidate.jwt = token;
+            context.candidate = Promise.resolve(candidate);
+            return candidate;
+          })
+          /*return bcrypt.hash(password, 10).then(hash =>
+            Candidate.create({
+            phone,
+            password: hash,
+            name: {
+              first: firstName,
+              last: lastName
+            }
+          })).then((candidate) => {
+            const { id, phone } = candidate;
+            console.log('---' + hash);
+            const token = jwt.sign({
+              id: candidate.id,
+              //email: candidate.email,
+              phone: candidate.phone,
+              type: 'Candidate',
+              //passwordVersion: candidate.passwordVersion,
+            }, process.env.JWT_SECRET);
+            console.log('-----' + candidate.password);
+            candidate.jwt = token;
+            context.candidate = Promise.resolve(candidate);
+            return candidate;
+          });*/
+        }
+        return Promise.reject('phone already Exists');
+      })
+    },
+  })
 }
