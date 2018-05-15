fdf3124fe0b35a6fa64d70e534b2624c01473e23
Bello Oladipupo <bellooladipupo41@gmail.com>
2018-05-15 13:34:08 +0100

    Update index.js

--- graphql/resolvers/index.js
+++ graphql/resolvers/index.js
@@ -1,6 +1,6 @@
 const candidateResolvers = require('./candidate')
 const companyResolvers = require('./company')
-const CenterManagerResolvers = require('./CenterManager')
+const CenterManagerResolvers = require('./centerManager')
 
 module.exports = addResolvers = () => {
   candidateResolvers();
