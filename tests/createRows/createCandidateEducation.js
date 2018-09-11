8ce093d68393887508ca347c383f3d1b335e41a3
Bello Oladipupo <bellooladipupo41@gmail.com>
2018-09-11 13:54:29 +0100

    Update createCandidateEducation test helper

--- tests/createRows/createCandidateEducation.js
+++ tests/createRows/createCandidateEducation.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-multi-assign,prefer-const */
 const keystone = require('keystone');
 
-const { STATES } = require('../../lib/common');
+// const { STATES } = require('../../lib/common');
 
 const Education = keystone.list('Education').model;
 
@@ -13,8 +13,8 @@ module.exports = async (payload = {}) => {
     degree: 'Example degree',
     field: 'Example field',
     grade: '1st Class',
-    state: STATES[n],
-    salary: 10000 + n,
+    // state: STATES[n],
+    // salary: 10000 + n,
     isSchoolingHere: true,
     fromYear: '2016',
     // toYear: '2019',
