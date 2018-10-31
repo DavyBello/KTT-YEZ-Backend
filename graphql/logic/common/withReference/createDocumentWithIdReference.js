372911f989cec2b1595dc1b48b6703063a018e37
Bello Oladipupo <bellooladipupo41@gmail.com>
2018-10-31 15:45:40 +0100

    Update createDocument higher order component to accept a post complete hook

--- graphql/logic/common/withReference/createDocumentWithIdReference.js
+++ graphql/logic/common/withReference/createDocumentWithIdReference.js
@@ -1,11 +1,15 @@
-module.exports = ({ TC, refPath }) => TC.get('$createOne').wrapResolve(next => async (rp) => {
+module.exports = ({ TC, refPath, onCompleted }) => TC.get('$createOne').wrapResolve(next => async (rp) => {
   // get viewer from resolveParams (rp)
   const { context: { viewer } } = rp;
   if (TC.hasField(refPath)) {
     rp.args.record[refPath] = viewer._id;
     rp.args.record.createdAt = Date.now();
     // run createOne resolver
-    return next(rp);
+    const result = await next(rp);
+    if (onCompleted && typeof onCompleted === 'function') {
+      onCompleted(result, rp);
+    }
+    return result;
   }
   return Promise.reject(Error('invalid refPath'));
 });
